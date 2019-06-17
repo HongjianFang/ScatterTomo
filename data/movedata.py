19df847df807a39743c4d66e40723d0eb919ac45
Hongjian Fang <fanghj1990@gmail.com>
2019-06-17 10:28:24 -0400

    all modules ready

--- data/movedata.py
+++ data/movedata.py
@@ -6,14 +6,16 @@
 #
 # Creation Date : 01-06-2019
 #
-# Last Modified : Sat Jun  1 18:10:30 2019
+# Last Modified : Mon Jun 17 10:19:18 2019
 #
 # Created By : Hongjian Fang: hfang@mit.edu 
 #
 #_._._._._._._._._._._._._._._._._._._._._.*/
 import os
 nsrc = 30
+datadir = 'wfs_25'
+if not os.path.isdir(datadir):
+  os.makedirs(datadir)
 for ii in range(nsrc):
   srcdir = '000'+str(ii).zfill(2)
-  os.makedirs(srcdir)
-  os.system('cp ../scratch/solver/'+srcdir+'/traces/obs/Uy_file_single.su '+srcdir)
+  os.system('cp ../scratch/solver/'+srcdir+'/traces/obs/Uy_file_single.su '+datadir+'/'+srcdir+'.su')
